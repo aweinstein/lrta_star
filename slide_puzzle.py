c303d1275dd2416a7f1e235305e638ca5c2a8a53
aweinstein <alejandr.weinstein@gmail.com>
2012-01-26 17:44:07 -0700

    Working version

--- slide_puzzle.py
+++ slide_puzzle.py
@@ -1,8 +1,11 @@
 from random import choice
 from copy import deepcopy
 
+def to_tuple_of_tuples(lol):
+    """Convert a list of lists into a tuple of tuples."""
+    return tuple(tuple(row) for row in lol)
+
 class SlidePuzzle(object):
-    left, right, up, down = range(4)
     def __init__(self, n=3):
 	grid = [[None] * n for _ in range(n)]
 	# For the moment a fix initial state
@@ -60,70 +63,105 @@ class SlidePuzzle(object):
         return acts
 
     def move(self, location):
+        self.grid, self.space = self.next_state(self.grid, self.space, 
+                                                location)
+        
+    def next_state(self, grid, space, location):
 	i, j = location
-	p, q = self.space
-	self.grid[i][j], self.grid[p][q] = self.grid[p][q], self.grid[i][j]
-	self.space = (i, j)
+	p, q = space
+	grid[i][j], grid[p][q] = grid[p][q], grid[i][j]
+	space = (i, j)
 
-    def shuffle(self):
-	n_moves = 1000
+        return grid, space
+
+    def shuffle(self, n_moves=1000):
 	for _ in range(n_moves):
 	    self.move(choice(self.actions()))
 
     def is_solved(self):
         return self.grid == self.goal_grid
 
-    def get_state(self):
-        return tuple(tuple(row) for row in self.grid)
+    def get_state(self, grid=None):
+        if grid is None:
+            grid = self.grid
+            #return tuple(tuple(row) for row in grid)
+        return to_tuple_of_tuples(grid)
 
-    def h(self):
-        """Return the heuristic cost of the current state.
+    def h(self, grid=None):
+        """Return the heuristic cost.
 
         Use the sum of the Manhattan distances from current position to the goal.
+
+        If `grid` is None, use the current state grid. Other wise use `grid`.
         """
+        if grid is None:
+            grid = self.grid
         h = 0
         for i in range(self.n):
             for j in range(self.n):
-                tile = self.grid[i][j]
+                tile = grid[i][j]
                 if tile:
                     i_goal, j_goal = self.tile_goals[tile]
                     h += abs(i - i_goal) + abs(j - j_goal)
         return h
                 
 
-def solve(puzzle):
+def solve(p):
     print 'Solving'
-    print puzzle
+    print p
     print
 
     u = {}
+    iters = 0
     while True:
-        if puzzle.is_solved():
-            print 'Solved!'
-            return
-        state = puzzle.get_state()
-        actions = puzzle.actions
-        # update u for the current state
+        if p.is_solved():
+            print 'Solved in %d steps!' % iters
+            return locals()
+        state = p.get_state()
+        if state not in u:
+            u[state] = p.h()
+        actions = p.actions()
+        succs = [p.next_state(deepcopy(p.grid), p.space, a) for a in actions]
+        m = float('inf')
+        a_min = None
+        for succ, a in zip(succs, actions):
+            grid_tot = to_tuple_of_tuples(succ[0])
+            if grid_tot not in u:
+                u[grid_tot] = p.h(succ[0])
+            if u[grid_tot] < m:
+                m = u[grid_tot]
+                a_min = a
+        u[state] = 1 + m
+        p.move(a_min)
+        print p
+        #raw_input()
+        if iters == 100000:
+            print 'Too many iterations :('
+            return locals()
+        iters += 1
+                
+
         
         
 def experiment_1():
-
     print 'Testing slide puzzle'
     puzzle = SlidePuzzle()
     print puzzle
     puzzle.shuffle()
-    print puzzle
     n_moves = 5
     print puzzle
     for _ in range(n_moves):
 	a = puzzle.actions()
 	puzzle.move(choice(a))
 	print puzzle
+    return locals()
 
 def experiment_2():
-    puzzle = SlidePuzzle()
-    puzzle.shuffle()
+    puzzle = SlidePuzzle(3)
+    puzzle.shuffle(1000)
     solve(puzzle)
+    return locals()
     
 if __name__ == '__main__':
-    experiment_1()
+    d = experiment_2()
+    locals().update(d)
